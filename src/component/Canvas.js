1ea8fe8c60d6baa4d8dcbca992f459537068fc19
mf-sourav <sourav.chatterjee@mindfiresolutions.com>
2021-03-30 14:17:08 +0530

    added interactive nature

--- src/component/Canvas.js
+++ src/component/Canvas.js
@@ -1,10 +1,20 @@
 import { useEffect, useRef } from "react"
-import { generateImageMap } from "../utils/calculator"
+import { generateImageMap, sleep } from "../utils/calculator"
 
 const Canvas = (props) => {
   const canvasRef = useRef(null)
-  const { width, height } = props
+  const { width, height, setDone } = props
   
+  const drawImage = async(context, data) => {
+    for (let i = 0; i < data.length; i++) {
+      let [ x, y, r, g, b ] = data[i]
+      context.fillStyle = `rgba(${r},${g},${b}, 1)`
+      context.fillRect( x, y, 1, 1 )
+      await sleep(0)
+    }
+    setDone(true)
+  }
+
   useEffect(()=>{
     const canvas = canvasRef.current
     canvas.width = width
@@ -13,11 +23,7 @@ const Canvas = (props) => {
     canvas.style.height = `${height}px`
     const context = canvas.getContext('2d')
     const { paint } = generateImageMap(width*height, width)
-    for (var i = 0; i < paint.length; i++) {
-      let [ x, y, r, g, b ] = paint[i]
-      context.fillStyle = `rgba(${r},${g},${b}, 1)`
-      context.fillRect( x, y, 1, 1 )
-    }
+    drawImage(context,paint)
   },[])
 
   return(
